9718752c78ef82a0c64bdab5a65bacdec6bf4864
jmj <jmj@iwaz.co.kr>
2024-10-24 11:21:10 +0900

    registchange

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -6,28 +6,33 @@ import api from "../utils/api";
 
 const RegisterPage = () => {
 
-  const [name, setName] = useState('');  // useState로 수정
+  const [name, setName] = useState('');  
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [secPassword, setSecPassword] = useState('');  // useState로 수정
+  const [secPassword, setSecPassword] = useState('');  
+  const [error, setError] = useState(''); // setError 추가
   const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {  // 매개변수로 event 추가
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      if (!email) {
-        throw new Error("이메일을 입력해주세요.");
-      }
       if (!name) {
         throw new Error("이름을 입력해주세요.");
       }
+
+      if (!email) {
+        throw new Error("이메일을 입력해주세요.");
+      }
+
       if (!password || !secPassword) {
          throw new Error("비밀번호를 입력해주세요.");
       }
-      if (password !== secPassword) {  // === 또는 !== 사용
-         throw new Error("패스워드가 일치하지 않습니다. 다시 입력해주세요");
+
+      if (password !== secPassword) { 
+        throw new Error("비밀번호가 일치하지 않습니다. 다시 입력해주세요");
       }
+
       const response = await api.post('/user', { name, email, password });
       if (response.status === 200) {
         navigate('/login');
@@ -35,12 +40,13 @@ const RegisterPage = () => {
         throw new Error(response?.data?.error || "서버에서 응답이 없습니다.");
       }
     } catch (error) {
-      console.error(error.message);  // 에러 처리
+      setError(error.message);  // 에러 메시지를 설정합니다.
     }
   };
 
   return (
     <div className="display-center">
+      {error && <div className="red-error">{error}</div>} {/* 에러 메시지 표시 */}
       <Form className="login-box" onSubmit={handleSubmit}>
         <h1>회원가입</h1>
         <Form.Group className="mb-3" controlId="formName">
